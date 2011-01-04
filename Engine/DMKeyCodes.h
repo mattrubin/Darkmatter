eeb401773f4bdaea9953c57ae5d4e0f80cf6c3d1
Matt Rubin <mattrubin@me.com>
2011-01-04 14:44:12 -0800

    Added key code constants

---
+++ Engine/DMKeyCodes.h
@@ -0,0 +1,140 @@
+//
+//  DMKeyCodes.h
+//  Darkmatter
+//
+//  Created by Matt Rubin on 1/2/11.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#define KEY_A					0x00
+#define KEY_S					0x01
+#define KEY_D					0x02
+#define KEY_F					0x03
+#define KEY_H					0x04
+#define KEY_G					0x05
+#define KEY_Z					0x06
+#define KEY_X					0x07
+#define KEY_C					0x08
+#define KEY_V					0x09
+//								0x0a
+#define KEY_B					0x0b
+#define KEY_Q					0x0c
+#define KEY_W					0x0d
+#define KEY_E					0x0e
+#define KEY_R					0x0f
+#define KEY_Y					0x10
+#define KEY_T					0x11
+#define KEY_1					0x12
+#define KEY_2					0x13
+#define KEY_3					0x14
+#define KEY_4					0x15
+#define KEY_6					0x16
+#define KEY_5					0x17
+#define KEY_EQUAL_SIGN			0x18
+#define KEY_9					0x19
+#define KEY_7					0x1a
+#define KEY_HYPHEN				0x1b
+#define KEY_8					0x1c
+#define KEY_0					0x1d
+#define KEY_CLOSE_BRACKET		0x1e
+#define KEY_O					0x1f
+#define KEY_U					0x20
+#define KEY_OPEN_BRACKET		0x21
+#define KEY_I					0x22
+#define KEY_P					0x23
+#define KEY_RETURN				0x24 // Duplicate
+#define KEY_ENTER				0x24 // Duplicate
+#define KEY_L					0x25
+#define KEY_J					0x26
+#define KEY_QUOTE				0x27
+#define KEY_K					0x28
+#define KEY_SEMICOLON			0x29
+#define KEY_BACKSLASH			0x2a
+#define KEY_COMMA				0x2b
+#define KEY_SLASH				0x2c
+#define KEY_N					0x2d
+#define KEY_M					0x2e
+#define KEY_PERIOD				0x2f
+#define KEY_TAB					0x30
+#define KEY_SPACEBAR			0x31
+#define KEY_GRAVE_ACCENT		0x32 // Duplicate
+#define KEY_TILDE				0x32 // Duplicate
+#define KEY_DELETE				0x33
+//								0x34
+#define KEY_ESCAPE				0x35
+#define KEY_RIGHT_COMMAND		0x36 //?
+#define KEY_LEFT_COMMAND		0x37 //?
+#define KEY_LEFT_SHIFT			0x38 //?
+//								0x39
+#define KEY_LEFT_ALT			0x3a //?
+#define KEY_LEFT_CONTROL		0x3b //?
+#define KEY_RIGHT_SHIFT			0x3c //?
+#define KEY_RIGHT_ALT			0x3d //?
+#define KEY_RIGHT_CONTROL		0x3e //?
+//								0x3f
+#define KEY_F17					0x40 //?
+#define KEYPAD_PERIOD			0x41 //?
+//								0x42
+#define KEYPAD_ASTERISK			0x43 //?
+//								0x44
+#define KEYPAD_PLUS				0x45 //?
+//								0x46
+//								0x47
+//								0x48
+//								0x49
+//								0x4a
+#define KEYPAD_SLASH			0x4b //?
+#define KEYPAD_ENTER			0x4c //?
+//								0x4d
+#define KEYPAD_HYPHEN			0x4e //?
+#define KEY_F18					0x4f //?
+#define KEY_F19					0x50 //?
+//								0x51
+#define KEYPAD_0				0x52 //?
+#define KEYPAD_1				0x53 //?
+#define KEYPAD_2				0x54 //?
+#define KEYPAD_3				0x55 //?
+#define KEYPAD_4				0x56 //?
+#define KEYPAD_5				0x57 //?
+#define KEYPAD_6				0x58 //?
+#define KEYPAD_7				0x59 //?
+//								0x5a
+#define KEYPAD_8				0x5b //?
+#define KEYPAD_9				0x5c //?
+//								0x5d
+//								0x5e
+//								0x5f
+#define KEY_F5					0x60
+#define KEY_F6					0x61
+#define KEY_F7					0x62
+#define KEY_F3					0x63
+#define KEY_F8					0x64
+//								0x65
+//								0x66
+//								0x67
+//								0x68
+#define KEY_F13					0x69 //?
+#define KEY_F16					0x6a //?
+#define KEY_F14					0x6b //?
+//								0x6c
+//								0x6d
+//								0x6e
+//								0x6f
+//								0x70
+#define KEY_F15					0x71 //?
+#define KEY_HELP				0x72 //?
+#define KEY_HOME				0x73 //?
+#define KEY_PAGE_UP				0x74 //?
+#define KEY_DELETE_FORWARD		0x75 //?
+#define KEY_F4					0x76
+#define KEY_END					0x77 //?
+#define KEY_F2					0x78
+#define KEY_PAGE_DOWN			0x79 //?
+#define KEY_F1					0x7a
+#define KEY_LEFT_ARROW			0x7b
+#define KEY_RIGHT_ARROW			0x7c
+#define KEY_DOWN_ARROW			0x7d
+#define KEY_UP_ARROW			0x7e
+//								0x7f
