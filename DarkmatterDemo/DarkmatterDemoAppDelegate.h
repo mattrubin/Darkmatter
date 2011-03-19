5257a6cd87a24ece96256120a8413c5e1be8e727
Matt Rubin <mattrubin@me.com>
2011-03-18 23:51:49 -0400

    Moved the MyGame and DarkmatterAppDelegate files into DarkmatterDemo

--- DarkmatterDemo/DarkmatterDemoAppDelegate.h
+++ DarkmatterDemo/DarkmatterDemoAppDelegate.h
@@ -1,18 +1,20 @@
 //
 //  DarkmatterDemoAppDelegate.h
-//  DarkmatterDemo
+//  Darkmatter
 //
-//  Created by Matt Rubin on 3/18/11.
-//  Copyright 2011 Matt Rubin. All rights reserved.
+//  Created by Matt Rubin on 12/26/10.
+//  Copyright 2010. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
+#import "GameWindow.h"
 
-@interface DarkmatterDemoAppDelegate : NSObject <NSApplicationDelegate> {
-@private
-	NSWindow *window;
+@interface DarkmatterDemoAppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate> {
+	GameWindow *mainWindow;
 }
 
-@property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet GameWindow *mainWindow;
+
+- (IBAction)toggleFullscreen:(id)sender;
 
 @end
--- DarkmatterDemo/DarkmatterDemoAppDelegate.m
+++ DarkmatterDemo/DarkmatterDemoAppDelegate.m
@@ -1,20 +1,47 @@
 //
 //  DarkmatterDemoAppDelegate.m
-//  DarkmatterDemo
+//  Darkmatter
 //
-//  Created by Matt Rubin on 3/18/11.
-//  Copyright 2011 Matt Rubin. All rights reserved.
+//  Created by Matt Rubin on 12/26/10.
+//  Copyright 2010. All rights reserved.
 //
 
 #import "DarkmatterDemoAppDelegate.h"
 
 @implementation DarkmatterDemoAppDelegate
 
-@synthesize window;
+@synthesize mainWindow;
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-	// Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	[self.mainWindow center];
+	[[NSNotificationCenter defaultCenter] addObserver:self.mainWindow selector:@selector(exitFullscreen) name:@"ExitFullscreen" object:nil];
 }
+
+- (IBAction)toggleFullscreen:(id)sender
+{
+	[mainWindow toggleFullscreen];
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)app
+{
+    return YES;
+}
+
+- (BOOL)windowShouldClose:(id)sender {
+	NSAlert *alert = [[[NSAlert alloc] init] autorelease];
+	[alert setMessageText:@"Are you sure you want to quit?"];
+	[alert setInformativeText:@"If you quit now, your game may not be saved."];
+	[alert addButtonWithTitle:@"OK"];
+	[alert addButtonWithTitle:@"Cancel"];
+	[alert setAlertStyle:NSWarningAlertStyle];
+	[alert beginSheetModalForWindow:mainWindow modalDelegate:self didEndSelector:@selector(alertDidEnd:returnCode:contextInfo:) contextInfo:nil];
+	return NO;
+}
+
+- (void) alertDidEnd:(NSAlert *)alert returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo {
+	if(returnCode==1000)
+		[mainWindow close];
+}
+
 
 @end
